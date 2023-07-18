3818105897e95f8345895938cec995a4284103fd
tareeb <67794123+tareeb@users.noreply.github.com>
2023-07-18 16:29:50 +0500

    Update apiConfig.js

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'http://ec2-16-170-211-2.eu-north-1.compute.amazonaws.com:8000/';
+export const API_BASE_URL = 'https://ec2-16-170-211-2.eu-north-1.compute.amazonaws.com/';
 
